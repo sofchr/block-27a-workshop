cebd8acde9aed5fdf2bf7d90b2a5c20afae6734e
Sofie Christensen <sofiec510@gmail.com>
2023-08-10 23:38:57 -0400

    persistent drawer, data table

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+// import DataGrid from "./components/DataGrid";
+import Transactions from "./components/Transactions";
+import PersistentDrawer from "./components/PersistentDrawer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <PersistentDrawer />
+      <Transactions />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Transactions.jsx
@@ -0,0 +1,37 @@
+import { mockTransactions } from "../Data";
+// import Title from "./Title";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
+export default function Transactions() {
+  return (
+    <div id="scroll">
+      <>
+        {/* <Title>Transactions</Title> */}
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>txId</TableCell>
+              <TableCell>User</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Cost</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {mockTransactions.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.txId}</TableCell>
+                <TableCell>{row.user}</TableCell>
+                <TableCell>{row.date}</TableCell>
+                <TableCell>{row.cost}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </>
+    </div>
+  );
+}
